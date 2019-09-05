48b09dff2f477e320b67065749e4ee8a66fba75a
Akash Pati <apati@vmware.com>
2019-09-05 13:15:00 +0530

    Completed test for login and user join

--- src/test/java/com/akash/chatroom/WebSocketChatApplicationTest.java
+++ src/test/java/com/akash/chatroom/WebSocketChatApplicationTest.java
@@ -1,17 +1,40 @@
 package com.akash.chatroom;
 
+import javafx.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(WebSocketChatApplication.class)
 public class WebSocketChatApplicationTest {
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testUserJoin() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/index").param("username", "apati"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
